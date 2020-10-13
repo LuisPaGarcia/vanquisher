9e804a0b6a792d3a7ede1bddfb163c0f54133b3e
LuisPa <diganluispa@gmail.com>
2020-10-12 20:29:57 -0600

    feat(app): Moved logic to app directory (#61)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import './App.scss';
-import Landing from './components/landing/';
-import Dashboard from './components/dashboard';
-import NotFound from './components/NotFound';
-import Callback from './components/Callback';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import ProtectedRoute from './components/ProtectedRoute';
+import './assets/main.css'
+import React from 'react'
+import Landing from './app/landing/'
+import Dashboard from './app/dashboard'
+// import NotFound from './components/NotFound';
+// import Callback from './components/Callback';
+// import ProtectedRoute from './components/ProtectedRoute'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App(props) {
-	return (
-		<Router>
-			<Switch>
-				<Route path="/" exact component={Landing} />
-				<Route path="/callback" component={Callback} />
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/dashboard" component={Dashboard} />
+        {/* <Route path="/callback" component={Callback} />
 				<ProtectedRoute path="/dashboard" component={() => <Dashboard {...props} />} {...props} />
-				<Route path="*" component={NotFound} />
-			</Switch>
-		</Router>
-	);
+				<Route path="*" component={NotFound} /> */}
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
